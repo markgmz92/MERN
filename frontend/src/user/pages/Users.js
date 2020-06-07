62bcd333e13cd608df57bfa6556e018c94a1f58b
markgmz92 <markgmz92@gmail.com>
2020-06-07 19:58:10 +0800

    Added userlist page

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <h2>Users Works!</h2>
+    const USERS = [{id: 'u1', name: 'John Doe', image: 'https://previews.123rf.com/images/alphaspirit/alphaspirit1504/alphaspirit150400118/38665682-simple-young-man-face-smiling-and-optimistic.jpg', places: 1}];
+    return <UsersList items={USERS}/>
 };
 
 export default Users;
