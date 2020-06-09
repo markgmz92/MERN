6414e2ffc80e9e32710df2db68a8ade3ab557c87
markgmz92 <markgmz92@gmail.com>
2020-06-09 16:57:15 +0800

    added map  feature

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -2,7 +2,8 @@ import React, {useState } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements//Modal';
+import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
@@ -15,7 +16,7 @@ const PlaceItem = props => {
     return <React.Fragment>
             <Modal show={showMap} onCancel={closeMapHandler} header={props.address} contentClass="place-item__modal-content" footerClass="place-item_modal-actions" footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
                 <div className="map-container">
-                    <h2>THE MAP!</h2>
+                    <Map center={props.coordinates} zoom={16} />
                 </div>
             </Modal>
             <li className="place-item">
